cd0357b65db8e79a245f315b8f9e59cf8b92b7b0
Jefferson Nuñez <jecfmaatrixxx@gmail.com>
2022-05-17 17:19:38 -0500

    Queues_v1

--- 06 - LinkedList/06-LinkedList.js
+++ 06 - LinkedList/06-LinkedList.js
@@ -5,7 +5,7 @@ const { parse, stringify } = require("flatted/cjs");
 // Este metodo deberia retornar la cantidad de elementos de la lista
 LinkedList.prototype.size = function () {
   // Tu código acá
-
+  return this.len;
 };
 
 // Agregar el método orderList al prototipo de LinkedList. Este método deberá ordenar los elementos de nuestra lista enlazada de mayor a menor.
@@ -16,8 +16,21 @@ LinkedList.prototype.size = function () {
 // ACLARACIÓN: Se debe ordenar la lista original y no una nueva.
 LinkedList.prototype.orderList = function () {
   // Tu código aca:
-  
+  let arr = [];
+  let current = this.head;
+  let next = current.next;
+  while (current) {
+    current = current.next;
+  }
 };
+
+let list = new LinkedList();
+list.add(4);
+list.add(1);
+list.add(6);
+list.add(2);
+//console.log(JSON.stringify(list));
+list.orderList();
 
 // agregar el metodo insert al prototipo de LinkedList.
 // Este metodo deberia recibir una posicion y un valor
@@ -25,19 +38,60 @@ LinkedList.prototype.orderList = function () {
 // tomar el head como posicion 1
 
 LinkedList.prototype.insert = function (data, pos) {
-  
+  let nodeInstert = new Node(data);
+  let current = this.data;
+  if (!this.head) return false;
+  if (this.len < pos && this.len === 1) {
+    return this.add(data);
+  } else if (pos === 1) {
+    nodeInstert.next = current;
+    this.head = nodeInstert;
+    this.len++;
+  } else {
+    while (pos - 1 > 1) {
+      pos--;
+      current = current.next;
+    }
+    nodeInstert.next = current.next;
+    current.next = nodeInstert;
+    this.len++;
+  }
 };
 
 const miListitaEnlazada = new LinkedList();
 miListitaEnlazada.add(33);
 miListitaEnlazada.add(44);
-miListitaEnlazada.add(22);
 miListitaEnlazada.add(66);
 console.log(miListitaEnlazada);
-console.log(JSON.stringify(miListitaEnlazada));
-miListitaEnlazada.insert(777, 1);
+miListitaEnlazada.insert(2, 1);
 console.log(miListitaEnlazada);
-console.log(stringify(miListitaEnlazada));
+
+//console.log(JSON.stringify(miListitaEnlazada));
+miListitaEnlazada.insert(777, 1);
+//console.log(miListitaEnlazada);
+//console.log(stringify(miListitaEnlazada));
+
+LinkedList.prototype.orderList = function () {
+  // Tu código aca:
+  var current = this.head;
+  var aux = null;
+  
+  if(!current)return null;
+
+  while(current !== null){
+    index = current.next;
+  while(index !== null){
+    if(current.value < index.value){
+       aux = current.value;
+       current.value = index.value;
+       index.value = aux;
+    }
+    index = index.next; 
+  }
+    current = current.next;
+  }
+ 
+};
 
 // EXTRAS ---------------------------------------------------------------------------------------------------------------------
 // En los ejercicios extras no contamos con los tests, por lo que no podemos comprobar que funcione correctamente
@@ -66,10 +120,7 @@ console.log(stringify(miListitaEnlazada));
 
 LinkedList.prototype.changeNotNumbers = function () {
   // Tu código aca:
-  
 };
-
-
 
 module.exports = {
   LinkedList,
